4aea62fc8097e76a792654b0bb48351286cf1f3e
Saheen Alam Shuvo <saheenshuvo182@gmail.com>
2024-09-23 01:49:21 +0600

    Noakhali donation done

---
+++ js/donateNoakhali.js
@@ -0,0 +1,31 @@
+document
+  .getElementById("btn-donate-now")
+  .addEventListener("click", function (event) {
+    event.preventDefault();
+
+// Money Donation Input
+    const donateMoney = getInputFieldValueById("input-donation-amount");
+    // NaN verify
+    if (isNaN(donateMoney)) {
+      alert("Failed to Cash Out.");
+      return;
+    }
+    console.log("Donate money with parameter", donateMoney);
+
+    // Balance update after donation
+    const balance = getTextFieldValueById("account-balance");
+    console.log("Account Balance with parameter", balance);
+    const newBalance = balance - donateMoney;
+    // Prevent Negative Balance
+    if (donateMoney > balance) {
+      alert("You have insufficient balance.");
+      return;
+    }
+    document.getElementById("account-balance").innerText = newBalance;
+
+    // Noakhali Fund update after donation
+    const noakhaliFund = getTextFieldValueById("noakhali-total-donation");
+    console.log("Noakhali Fund with parameter", noakhaliFund);
+    const newFund = noakhaliFund + donateMoney;
+    document.getElementById("noakhali-total-donation").innerText = newFund;
+  });
