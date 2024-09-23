08a9fd51115a5031d0e7d25f3ba3ae5de489e497
Saheen Alam Shuvo <saheenshuvo182@gmail.com>
2024-09-23 10:06:32 +0600

    Quota donation works

---
+++ js/donateQuota.js
@@ -0,0 +1,31 @@
+document
+  .getElementById("btn-donate-quota")
+  .addEventListener("click", function (event) {
+    event.preventDefault();
+
+// Money Donation Input
+    const donateMoney = getInputFieldValueById("input-donate-quota");
+    // NaN verify
+    if (isNaN(donateMoney)) {
+      alert("Failed to Cash Out.");
+      return;
+    }
+    console.log("Donate money with parameter", donateMoney);
+
+    // Balance update after donation
+    const balance = getTextFieldValueById("account-balance");
+    console.log("Account Balance with parameter", balance);
+    const newBalance = balance - donateMoney;
+    // Prevent Negative Balance
+    if (donateMoney > balance) {
+      alert("You have insufficient balance.");
+      return;
+    }
+    document.getElementById("account-balance").innerText = newBalance;
+
+    // Quota Fund update after donation
+    const quotaFund = getTextFieldValueById("quota-total-donation");
+    console.log("Quota Fund with parameter", quotaFund);
+    const newFund = quotaFund + donateMoney;
+    document.getElementById("quota-total-donation").innerText = newFund;
+  });
